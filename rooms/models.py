5a7438045edd205ab67202ba2f7f76d80c987b7d
박대준 <jun@daejunpark-ui-MacBookPro.local>
2022-12-28 16:48:14 +0900

    add Room and Amenity Model

---
+++ rooms/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from users.models import User
+
+from commons.models import Common
+
+
+class Amenity(Common):
+    """Amenity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Room(models.Model):
+
+    """Room Model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = ("shared_room", "Shared Room")
+
+    name = models.CharField(max_length=140)
+
+    country = models.CharField(
+        max_length=50,
+        #    default="Korea",
+    )
+    city = models.CharField(max_length=80)
+    address = models.CharField(max_length=250)
+
+    price = models.PositiveIntegerField()
+
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    amenities = models.ManyToManyField("rooms.Amenity", related_name="rooms")
+
+    pet_friendly = models.BooleanField(default=True)
+
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="rooms",
+    )
+
+    # reviews, categories ...
+
+    def __str__(self) -> str:
+        return self.name
+
+    def total_amenities(self) -> int:
+        return self.amenities.count()
