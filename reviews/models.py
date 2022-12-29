03e8f3b9c10c1cbb509f32ea2a74c0cdc853c511
박대준 <jun@daejunpark-ui-MacBookPro.local>
2022-12-29 10:13:22 +0900

    add Review model

---
+++ reviews/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from commons.models import Common
+
+
+class Review(Common):
+    """Review model definition"""
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="reivews",
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+    # TODO : add experience
+    comment = models.TextField()
+
+    # TODO : validate input value from 0 to 5
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user} give {self.rating} to {self.room}"
