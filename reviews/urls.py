541b48949a480f79eb6cc7ba0ee40975650409e9
박대준 <jun@daejunpark-ui-MacBookPro.local>
2023-01-01 21:57:13 +0900

    add review api for room reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import MyReviewsAPIView
+from .views import MyReviewsAPIView, RoomReviewAPIView
 
 urlpatterns = [
     path("My", MyReviewsAPIView.as_view()),
+    path("RoomReview/<int:pk>", RoomReviewAPIView.as_view()),  # TODO : url.
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 from .serializers import ReviewSerializer
+
+from rooms.models import Room
 
 # for testing.
 class MyReviewsAPIView(APIView):
@@ -15,3 +18,21 @@ class MyReviewsAPIView(APIView):
         return Response(
             ReviewSerializer(request.user.reviews.all(), many=True).data,
         )
+
+
+class RoomReviewAPIView(APIView):
+    """Room Review API"""
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_room(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_room(pk)
+        # TODO : pagination
+        serializer = ReviewSerializer(room.reviews.all(), many=True)
+        return Response(serializer.data)
