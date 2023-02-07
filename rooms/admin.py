3c33cf071b822e5e1635e258d831b8518f27e594
chestnut90 <cowzon90@gmail.com>
2023-02-07 12:39:14 +0900

    modify rooms adminpanel

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,11 +5,38 @@ from . import models
 
 @admin.register(models.Amenity)
 class AmenityAdmin(admin.ModelAdmin):
-
-    list_display = ("name", "description", "created_at", "updated_at")
-    pass
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
