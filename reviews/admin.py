2206a2d3be0b4426fb6c71e45de691b483a61982
chestnut90 <cowzon90@gmail.com>
2023-02-08 12:27:03 +0900

    add WordFilter(custom filter) into Review admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,6 +3,34 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, queryset):
+        word = self.value()
+        if word:
+            return queryset.filter(comment__contains=word)
+        else:
+            return queryset
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "__str__",
+        "comment",
+    )
+
+    list_filter = (
+        WordFilter,
+        "rating",
+        "room__pet_friendly",
+    )
